8b5afd31faaa1369a200d85e568592ce24a05ecf
Jordon LeBaron <Jordon LeBaron>
2017-02-08 21:19:33 -0700

    Attempt to add rotating pictures to JordonHQ

--- js/jordonHQ.js
+++ js/jordonHQ.js
@@ -1,3 +1,19 @@
 //document.onclick = function() {
 //	alert("Hello");
 //}
+
+var myImage = document.getElementByID("jordonHQImage");
+
+var imageArray = ["images/family14.jpg", "images/asherFun.jpg"];
+
+var imageIndex = 0;
+
+function changeImage() {
+	myImage.setAttribute("src", imageArray[imageIndex]);
+	imageIndex++;
+	if (imageIndex >= imageArray.length) {
+		imageIndex = 0;
+	}
+}
+
+setInterval(changeImage, 4000);
