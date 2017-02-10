70e6be47cbb2566eade84652e34094318172c19d
Jordon LeBaron <Jordon LeBaron>
2017-02-09 21:46:38 -0700

    Javascript working -- Rotating Pictures working

--- js/jordonHQ.js
+++ js/jordonHQ.js
@@ -3,7 +3,7 @@
 //}
 
 
-//Image Change Code --- I can't get this to work yet
+//Image Change Code --- need to figure out picture orientation
 var myImage = document.getElementById("jordonHQImage");
 
 var imageArray = ["images/family14.jpg", "images/asherFun.jpg"];
@@ -21,10 +21,18 @@ function changeImage() {
 setInterval(changeImage, 4000);
 //End Image Change Code
 
-var foodTitle = document.getElementById("foodTitle");
-food.setAttribute("font-family", "Garamond");
 
-var foodAppend = document.createElement("li");
-foodTitle.appendChild(foodAppend);
-var foodText = document.createTextNode("Hello");
-foodAppend.appendChild(foodText);
+//Attempt to perform Javascript .getElementById call
+//var foodTitle = document.getElementById("foodTitle");  
+//food.setAttribute("font-family", "Garamond");          //Not used for style
+
+//var foodAppend = document.createElement("li");
+//foodTitle.appendChild(foodAppend);
+//var foodText = document.createTextNode("Hello");
+//foodAppend.appendChild(foodText);
+//end ATTEMPT
+
+var styleTest = document.getElementById("foodTitle");
+styleTest.style.color = "red";
+
+var picChange = document.getElementById("Jordon")
